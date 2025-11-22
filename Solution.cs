290d5b6e9e9645009de809e41bac95735ba64550
Виталий Гуляй <guliavitali@gmail.com>
2025-11-22 14:05:44 +0300

    Добавьте файлы проекта.

---
+++ Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Calculator
+{
+    internal class Solution
+    {
+        public static double Minus(string k, string a, string b, out string exMessage)
+        {
+            double a1 = CheckData.CheckDataToDouble(a, out exMessage);
+            double b1 = CheckData.CheckDataToDouble(b, out exMessage);
+
+            switch (k) {
+                case "+":
+                    return a1 + b1;
+                case "-":
+                    return a1 - b1;
+                case "*":
+                    return a1 * b1;
+                case "/":
+                    return a1 / b1;
+                default:
+                    exMessage = "Invalid value";
+                    return 0;
+            }
+        }
+    }
+}
